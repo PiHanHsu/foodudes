3c97bcf70ba1f47b2af4c4c9882c49c3d8b0306d
PiHan Hsu <pihanhsu@gmail.comgit config --global user.name PiHanHsu>
2014-11-19 15:58:22 +0800

    Initial Commit

---
+++ foodudes/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  foodudes
+//
+//  Created by PiHan Hsu on 2014/11/19.
+//  Copyright (c) 2014年 PiHan Hsu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ foodudes/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  foodudes
+//
+//  Created by PiHan Hsu on 2014/11/19.
+//  Copyright (c) 2014年 PiHan Hsu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
