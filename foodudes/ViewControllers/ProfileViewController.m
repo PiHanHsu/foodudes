622679a5aa6cec5fc6ecb84a2054f69637f66317
PiHan Hsu <pihanhsu@gmail.comgit config --global user.name PiHanHsu>
2014-11-24 15:21:32 +0800

    first push to github
    showing friends info and photo.

--- foodudes/ViewControllers/ProfileViewController.m
+++ foodudes/ViewControllers/ProfileViewController.m
@@ -12,7 +12,9 @@
 #import <ParseFacebookUtils/PFFacebookUtils.h>
 #import <FacebookSDK/FacebookSDK.h>
 
-@interface ProfileViewController ()
+@interface ProfileViewController ()<UITableViewDataSource, UITableViewDelegate>
+
+@property (weak, nonatomic) IBOutlet UITableView *friendsTableView;
 
 @end
 
@@ -70,10 +72,99 @@
 
 }
 }
+#pragma tableView
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView{
+    
+    return 1 ;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    return 10;
+    
+}
+
+- (CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath{
+    
+    return 60.0;
+}
+
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    static NSString *requestIdentifier = @"HelloCell";
+    static NSString *requestIdentifier2 = @"HelloCell2";
+    
+    UITableViewCell *cell;
+    switch (indexPath.section) {
+        case 0:{
+            cell = [tableView dequeueReusableCellWithIdentifier:requestIdentifier];
+            if (!cell) {
+                cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleSubtitle reuseIdentifier:requestIdentifier];
+                cell.textLabel.textColor = [UIColor whiteColor];
+                cell.backgroundColor = [UIColor clearColor];
+                cell.selectionStyle=UITableViewCellSelectionStyleNone;
+                cell.detailTextLabel.textColor = [UIColor colorWithRed:241.0f/255.0f green:244.0f/255.0f blue:239.0f/255.0f alpha:1.0f];
+                cell.textLabel.font = [UIFont fontWithName:@"AppleSDGothicNeo-Bold" size: 20.f];
+                cell.detailTextLabel.font = [UIFont fontWithName:@"Avenir-LightOblique" size: 12.f];
+                
+            }
+        }
+            break;
+        case 1:{
+            cell = [tableView dequeueReusableCellWithIdentifier:requestIdentifier2];
+            if (!cell) {
+                cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleSubtitle reuseIdentifier:requestIdentifier2];
+                cell.textLabel.textColor = [UIColor whiteColor];
+                cell.backgroundColor = [UIColor clearColor];
+                cell.selectionStyle=UITableViewCellSelectionStyleNone;
+                cell.detailTextLabel.textColor = [UIColor colorWithRed:241.0f/255.0f green:244.0f/255.0f blue:239.0f/255.0f alpha:1.0f];
+                cell.textLabel.font = [UIFont fontWithName:@"AppleSDGothicNeo-Bold" size: 20.f];
+                cell.detailTextLabel.font = [UIFont fontWithName:@"Avenir-LightOblique" size: 12.f];
+                
+            }
+        }
+        default:
+            break;
+    }
+    /*
+    cell.textLabel.text = dataSource [indexPath.row];
+    cell.detailTextLabel.text = detailDataSource [indexPath.row];
+    
+    NSString *title;
+    switch (indexPath.section) {
+        case 0:
+            title = @"download";
+            break;
+        case 1:
+            title =@"upload";
+            break;
+            
+        default:
+            break;
+    }
+    
+    UIButton *button= [[ UIButton alloc] initWithFrame:CGRectMake(220, 15, 80, 30)];
+    [button setTitle:title forState:UIControlStateNormal];
+    button.titleLabel.font = [UIFont fontWithName:@"AppleSDGothicNeo-Bold" size: 14.f];
+    [button setBackgroundColor:[UIColor clearColor]];
+    [button addTarget:self action:@selector(buttonPressed:) forControlEvents:UIControlEventTouchUpInside];
+    button.tag =indexPath.row;
+    //button.layer.cornerRadius = 25.0f;
+    [button.layer setCornerRadius:4.0f];
+    [button.layer setBorderColor:[[UIColor whiteColor]CGColor]];
+    [button.layer setBorderWidth:1.0f];
+    [cell.contentView addSubview:button];
+    */
+    return cell;
+}
+
+#pragma logout
+
 - (IBAction)logoutButtonPressed:(id)sender {
     
     [PFUser logOut];
